0aa771cd362f98c63c50397a5f4c285c5c9ecc83
Ayush Agarwal <ayushagarwal2500@gmail.com>
2025-01-05 18:23:23 -0500

    Revert "Revert "separate headed dino""
    This reverts commit 8f4e98dba2259c2088a33a67d8f1e4fd8f9d1fe8.

--- quest/algos/utils/rgb_modules.py
+++ quest/algos/utils/rgb_modules.py
@@ -368,6 +368,7 @@ class DINOEncoder(nn.Module):
     def __init__(
         self,
         input_shape,
+        output_size,
         freeze=True,
     ):
         super().__init__()
@@ -384,38 +385,6 @@ class DINOEncoder(nn.Module):
             ]
         )
         self.dino = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
-        # self.mlp_block = nn.Sequential(
-        #     nn.Linear(384, 64),
-        #     nn.GELU(),
-        #     nn.Dropout(0.1),
-        #     nn.Linear(64, 1),
-        #     nn.Dropout(0.1),
-        # )
-        # self.projection = nn.Linear(384, output_size)
-        # self.out_channels = output_size
-        self.freeze = freeze    
-        if self.freeze:
-            for param in self.dino.parameters():
-                param.requires_grad = False
-
-    def forward(self, x, langs=None):
-        x = self.preprocess(x)
-        x = self.dino(x,is_training=True)
-        x = x['x_norm_patchtokens']
-        # mask = self.mlp_block(x).permute(0, 2, 1)
-        # mask = F.softmax(mask, dim=-1)
-        # x = torch.einsum('...si,...id->...sd', mask, x)
-        # x = torch.squeeze(self.projection(x), dim=1)
-        return x
-    
-class DINOProjection(nn.Module):
-    def __init__(
-        self,
-        input_shape,
-        output_size,
-    ):
-        super().__init__()
-
         self.mlp_block = nn.Sequential(
             nn.Linear(384, 64),
             nn.GELU(),
@@ -425,10 +394,18 @@ class DINOProjection(nn.Module):
         )
         self.projection = nn.Linear(384, output_size)
         self.out_channels = output_size
+        self.freeze = freeze    
+        if self.freeze:
+            for param in self.dino.parameters():
+                param.requires_grad = False
 
     def forward(self, x, langs=None):
+        x = self.preprocess(x)
+        x = self.dino(x,is_training=True)
+        x = x['x_norm_patchtokens']
         mask = self.mlp_block(x).permute(0, 2, 1)
         mask = F.softmax(mask, dim=-1)
         x = torch.einsum('...si,...id->...sd', mask, x)
-        x = self.projection(x)
+        x = torch.squeeze(self.projection(x), dim=1)
         return x
+    
